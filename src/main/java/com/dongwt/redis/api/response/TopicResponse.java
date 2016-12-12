f38234217a36d79a4df8cee03f2884aa59626c25
董 纹陶 [SVR III] <dongwentao@lifang.com>
2016-12-12 11:15:24 +0800

    push pop listener 调通

---
+++ src/main/java/com/dongwt/redis/api/response/TopicResponse.java
@@ -0,0 +1,20 @@
+package com.dongwt.redis.api.response;
+
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TopicResponse<T> implements Serializable{
+
+    private static final long serialVersionUID = 1L;
+    
+    private String UUID;
+    
+    private T body;
+
+}
